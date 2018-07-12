59e4e5c42aabe979779ceda38c2f30fd97ca4d07
Riswana <riswanafarween.m@payoda.com>
2018-07-12 15:43:20 +0530

    New controls added

--- assets/javascripts/cockpit.js
+++ assets/javascripts/cockpit.js
@@ -17,15 +17,49 @@ function postRequest(url,postData,callback, errorCallback){
         }
     });
 }
+
+function getRequest(url,callback, errorCallback){
+  $.ajax({
+      type: "GET",
+      beforeSend: function(request) {
+          request.setRequestHeader("Content-Type", 'application/json');
+      },
+      url: url,
+      data: "",
+      success: function(data) {
+          // console.log(JSON.stringify(data));
+          callback(data);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+          console.log('jqXHR:'+JSON.stringify(jqXHR));
+          console.log('textStatus:'+textStatus);
+          console.log('errorThrown:'+errorThrown);
+      }
+  });
+}
+
 var socket = io.connect(config.socketURl);
 
 var jsondata = new Object();
-jsondata = {"simid":config.simualtorUserid,"userid":config.cockpitUserid };
+jsondata = {"simid":config.simid,"userid":config.cockpitUserid };
 
 socket.on('connect', function(){
   console.log("socket client connected");
   socket.emit('adduser', jsondata);
+  getCurrentStateOfSimulator();
 });
+
+function getCurrentStateOfSimulator(){
+
+  var url = config.serverURL + config.currentstatus + "?simid=" + config.simid +"&userid=" + config.cockpitUserid;
+  getRequest(url, function(response) {
+    if(response.data) {
+
+    } else if(response.error) {
+      alert("Please on the simulator ");
+    }
+  });
+};
 
 socket.on('engine', function (data) {
   console.log("Listening engine "+JSON.stringify(data));
@@ -41,4 +75,76 @@ socket.on('speed', function (data) {
   console.log("Listening speed "+JSON.stringify(data));
   $("#listenSliderForDashboard").val(data.range);
   $('.output b').text(data.range);
+});
+
+socket.on('accessories', function (data) {
+  console.log("Listening accessories "+JSON.stringify(data));
+  $("#dashboardWifi").prop('checked',data.accessories.wifi);
+  $("#dashboardBt").prop('checked',data.accessories.bt);
+  $("#dashboardMedia").prop('checked',data.accessories.media);
+});
+
+socket.on('simstatus', function (data) {
+  console.log("Listening simstatus "+JSON.stringify(data));
+  $("#engineState").prop('checked',data.details.engine);
+  $("#lampState").prop('checked',data.details.lamp);
+  $("#listenSliderForDashboard").val(data.details.speed);
+  $('.output b').text(data.details.speed);
+  $("#dashboardWifi").prop('checked',data.details.accessories.wifi);
+  $("#dashboardBt").prop('checked',data.details.accessories.bt);
+  $("#dashboardMedia").prop('checked',data.details.accessories.media);
+});
+
+socket.on('hb', function (data) {
+  console.log("Listening hb "+JSON.stringify(data));
+  $("#dashboardHandBrake").prop('checked',data.status);
+});
+
+socket.on('indicator', function (data) {
+  console.log("Listening indicator "+JSON.stringify(data));
+  $("#dashboardLIndicator").prop('checked',data.details.left);
+  $("#dashboardRIndicator").prop('checked',data.details.right);
+});
+
+$("#dashboardWifi").change(function() {
+ console.log("wifi change called");
+  var wifiState = $("#dashboardWifi").is(":checked");
+  console.log(wifiState);
+  var postData = {
+    "simid": config.simid,
+    "userid": config.cockpitUserid,
+    "status" : wifiState
+  }
+  var url = config.serverURL + config.wifi;
+  postRequest(url, postData, function(response){
+  		console.log(response);
+  });
+});
+$("#dashboardMedia").change(function() {
+ console.log("Media change called");
+  var mediaState = $("#dashboardMedia").is(":checked");
+  console.log(mediaState);
+  var postData = {
+    "simid": config.simid,
+    "userid": config.cockpitUserid,
+    "status" : mediaState
+  }
+  var url = config.serverURL + config.media;
+  postRequest(url, postData, function(response){
+  		console.log(response);
+  });
+});
+$("#dashboardBt").change(function() {
+ console.log("Bluetooth change called");
+  var btState = $("#dashboardBt").is(":checked");
+  console.log(btState);
+  var postData = {
+    "simid": config.simid,
+    "userid": config.cockpitUserid,
+    "status" : btState
+  }
+  var url = config.serverURL + config.blueTooth;
+  postRequest(url, postData, function(response){
+  		console.log(response);
+  });
 });
--- assets/javascripts/simulator.js
+++ assets/javascripts/simulator.js
@@ -17,13 +17,68 @@ function postRequest(url,postData,callback, errorCallback){
         }
     });
 }
+var socket = io.connect(config.socketURl);
+
+var jsondata = new Object();
+jsondata = {"simid":config.simid,"userid":config.simualtorUserid };
+
+socket.on('connect', function(){
+  console.log("socket client connected");
+  socket.emit('adduser', jsondata);
+});
+
+socket.on('wifi', function (data) {
+  console.log("Listening wifi "+JSON.stringify(data));
+  $("#wifi").prop('checked',data.status);
+});
+
+socket.on('media', function (data) {
+  console.log("Listening media "+JSON.stringify(data));
+  $("#media").prop('checked',data.status);
+});
+
+socket.on('bt', function (data) {
+  console.log("Listening bt "+JSON.stringify(data));
+  $("#bt").prop('checked',data.status);
+});
+
+socket.on('currentstatus', function (data) {
+
+  var accessories = {
+    "wifi":$("#wifi").is(":checked"),
+    "bt":$("#bt").is(":checked"),
+    "media":$("#media").is(":checked")
+  };
+
+  var details = {
+    "engine":$("#engineControl").is(":checked"),
+    "lamp":$("#lampControl").is(":checked"),
+    "speed":$('.output b').text(),
+    "accessories":accessories
+  };
+
+  var postData = {
+    "simid": config.simid,
+    "userid": config.simualtorUserid,
+    "details" : details
+  };
+  console.log("current status of simulator");
+  console.log(postData);
+
+  var url = config.serverURL + config.simulatorstatus;
+  postRequest(url, postData, function(response){
+  		console.log(response);
+  });
+
+});
+
 $("#engineControl").change(function() {
  console.log("Engine control change called");
   var engineState = $(this).is(":checked");
   console.log(engineState);
   var postData = {
-    "simid": config.simualtorUserid,
-    "userid": config.cockpitUserid,
+    "simid": config.simid,
+    "userid": config.simualtorUserid,
     "status" : engineState
   }
   var url = config.serverURL + config.enginestatus;
@@ -38,11 +93,35 @@ $("#lampControl").change(function() {
   var lampState = $(this).is(":checked");
   console.log(lampState);
   var postData = {
-    "simid": config.simualtorUserid,
-    "userid": config.cockpitUserid,
+    "simid": config.simid,
+    "userid": config.simualtorUserid,
     "status" : lampState
   }
   var url = config.serverURL + config.lampstatus;
+  postRequest(url, postData, function(response){
+  		console.log(response);
+  });
+});
+
+$("#wifi,#bt,#media").change(function() {
+ console.log("wifi change called");
+  var wifiState = $("#wifi").is(":checked");
+  console.log(wifiState);
+  var btState = $("#bt").is(":checked");
+  console.log(btState);
+  var mediaState = $("#media").is(":checked");
+  console.log(mediaState);
+  var accessories = {
+    "wifi" : wifiState,
+    "bt": btState,
+    "media":mediaState
+  };
+  var postData = {
+    "simid": config.simid,
+    "userid": config.simualtorUserid,
+    "accessories" : accessories
+  }
+  var url = config.serverURL + config.accessoriesstatus;
   postRequest(url, postData, function(response){
   		console.log(response);
   });
@@ -53,12 +132,48 @@ $('#listenSlider').change(function() {
   var speedRange = this.value;
   console.log(speedRange);
   var postData = {
-    "simid": config.simualtorUserid,
-    "userid": config.cockpitUserid,
+    "simid": config.simid,
+    "userid": config.simualtorUserid,
     "range" : speedRange
   };
   var url = config.serverURL + config.runningstatus;
   postRequest(url, postData, function(response){
       console.log(response);
+  });
+});
+
+$("#handBrake").change(function() {
+ console.log("handBrake change called");
+  var handBrakeState = $(this).is(":checked");
+  console.log(handBrakeState);
+  var postData = {
+    "simid": config.simid,
+    "userid": config.simualtorUserid,
+    "status" : handBrakeState
+  }
+  var url = config.serverURL + config.handBrake;
+  postRequest(url, postData, function(response){
+  		console.log(response);
+  });
+});
+
+$("#lIndicator,#rIndicator").change(function() {
+ console.log("Indicator change called");
+  var lIndicatorState = $("#lIndicator").is(":checked");
+  console.log(lIndicatorState);
+  var rIndicatorState = $("#rIndicator").is(":checked");
+  console.log(rIndicatorState);
+  var details = {
+    "left":lIndicatorState,
+    "right":rIndicatorState
+  };
+  var postData = {
+    "simid": config.simid,
+    "userid": config.simualtorUserid,
+    "details" : details
+  }
+  var url = config.serverURL + config.indicator;
+  postRequest(url, postData, function(response){
+  		console.log(response);
   });
 });
